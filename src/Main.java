e0cd974977180ea8b157ef33a366eccc0225cb79
Eduardo <eduhenriquesrs@gmail.com>
2020-04-13 22:54:03 -0300

    teste

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+public class Main {
+    public static void main(String[] args){
+    	 int estado = 0;
+         
+         Lampada acao = new Lampada();
+        
+        estado = JOptionPane.showConfirmDialog(null, "Ligar a lampada");
+        if(estado == 0)
+            acao.ligar();
+        else
+            acao.desligar();
+        JOptionPane.showMessageDialog(null, acao.observar());
+    }   
+    }
